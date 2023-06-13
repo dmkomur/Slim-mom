1de7b696a973b6752efef5cd0f4f88ca414b3e0d
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-06-13 17:21:17 +0300

    add login and register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,51 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+let schema = yup.object({
+  password: yup
+    .string()
+    .required('Please enter a password')
+    .min(8)
+    .max(32)
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])/, 'a-z and 0-9'),
+
+  email: yup
+    .string()
+    .required('Please enter a password')
+    .email()
+    .min(8)
+    .max(32),
+});
+
 function Login() {
-  return <div>Login</div>;
+  const startValue = {
+    email: '',
+    password: '',
+  };
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+  };
+  return (
+    <>
+      <Formik
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+        initialValues={startValue}
+      >
+        <Form>
+          <lable htmlFor="email">Email</lable>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+
+          <lable htmlFor="password">Password</lable>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+
+          <button type="submit">Log in</button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,58 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+let schema = yup.object({
+  username: yup.string().required('Please enter a name').min(3).max(32),
+
+  password: yup
+    .string()
+    .required('Please enter a password')
+    .min(8)
+    .max(32)
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])/, 'a-z and 0-9'),
+
+  email: yup
+    .string()
+    .required('Please enter a password')
+    .email()
+    .min(8)
+    .max(32),
+});
+
 function Register() {
-  return <div>Register</div>;
+  const startValue = {
+    email: '',
+    password: '',
+    username: '',
+  };
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+  };
+  return (
+    <>
+      <Formik
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+        initialValues={startValue}
+      >
+        <Form>
+          <lable htmlFor="username">Name</lable>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" />
+
+          <lable htmlFor="email">Email</lable>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+
+          <lable htmlFor="password">Password</lable>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
 export default Register;
