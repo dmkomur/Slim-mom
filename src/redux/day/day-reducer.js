a83f15ae554714697b2a92b8fbe627acf41cc2bb
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-06-14 16:18:23 +0300

    added day reducer

---
+++ src/redux/day/day-reducer.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { postProduct, deleteProduct, dayInfo } from './day-operations';
+
+const initialState = {
+  id: null,
+  eatenProducts: [],
+  date: '',
+  daySummary: {},
+};
+
+export const daySlice = createSlice({
+  name: 'day',
+  initialState,
+  extraReducers: builder => {
+    builder.addCase(postProduct.fulfilled, (state, action) => {
+      state.eatenProducts = action.payload.day.eatenProducts;
+      state.id = action.payload.day.id;
+      state.date = action.payload.day.date;
+      state.daySummary = action.payload.daySummary;
+    });
+    builder.addCase(deleteProduct.fulfilled, (state, action) => {
+      state.daySummary = action.payload.newDaySummary;
+    });
+    builder.addCase(dayInfo.fulfilled, (state, action) => {
+      state.id = action.payload.id;
+      state.eatenProducts = action.payload.eatenProducts;
+      state.date = action.payload.date;
+      state.daySummary = action.payload.daySummary;
+    });
+  },
+});
