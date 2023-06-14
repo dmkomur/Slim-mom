55e1bb96227bb6d6f12d96089b95d2b6f6ad45ba
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-06-14 13:47:43 +0300

    updated selectors

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -3,3 +3,13 @@ export const getIsLoggedIn = state => state.auth.isLoggedIn;
 export const getIsRefreshing = state => state.auth.isRefreshing;
 
 export const getUser = state => state.auth.user;
+
+export const getDailyRate = state => state.user.userData.dailyRate;
+export const getNotAllowedProducts = state =>
+    state.user.userData.notAllowedProducts;
+  
+export const getDaily = state => {
+  const dailyRate = getDailyRate(state);
+  const notAllowedProducts = getNotAllowedProducts(state);
+  return { dailyRate, notAllowedProducts };
+};
