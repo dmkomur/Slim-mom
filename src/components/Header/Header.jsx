54954da7ae1200a2975be013edb0fa86009bd5bc
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-06-14 11:54:04 +0300

    added basic header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/auth-operations';
+
 function Header() {
-  return <div>Header</div>;
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/registration">Register</NavLink>
+      <NavLink to="/calculator">Calculator</NavLink>
+      <NavLink to="/diary">Diary</NavLink>
+      <button onClick={() => dispatch(logOut())} type="button">
+        Sign out
+      </button>
+    </div>
+  );
 }
 
 export default Header;
