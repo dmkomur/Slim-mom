e9808c261574c9192debc89cacaba90db789ab8b
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-06-14 17:07:57 +0300

    CreatDiary

--- src/components/DiaryProductListItem/DiaryProductListItem.jsx
+++ src/components/DiaryProductListItem/DiaryProductListItem.jsx
@@ -1,5 +1,38 @@
+import * as css from './DiaryProductListItem.styled';
+
 function DiaryProductListItem() {
-  return <div>DiaryProductListItem</div>;
+  return (
+    <>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+      <css.ListItem><css.PName>Eggplant</css.PName> <css.PGrame>100 g</css.PGrame> <css.PKcal>320 kcal</css.PKcal> <css.Button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none">
+        <path stroke="#9B9FAA" strokeWidth="2" d="m1 1 12 12M1 13 13 1" />
+      </svg></css.Button></css.ListItem>
+    </>
+
+  );
 }
 
 export default DiaryProductListItem;
