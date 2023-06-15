2e10a7b3d4a8a5a94947f53693695c1339af4fc0
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-06-15 11:32:20 +0300

    Update PageNotFound.jsx

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { PageNotFd } from './PageNotFound.styled';
 
+// const title = document.querySelector('h1');
+// // light
+// document.onmousemove = function (e) {
+//   let x = e.pageX - window.innerWidth / 2;
+//   let y = e.pageY - window.innerHeight / 2;
+
+//   title.style.setProperty('--x', x + 'px');
+//   title.style.setProperty('--y', y + 'px');
+// };
+// // shadow
+// title.onmousemove = function (e) {
+//   let x = e.pageX - window.innerWidth / 2;
+//   let y = e.pageY - window.innerHeight / 2;
+//   let rad = Math.atan2(y, x).toFixed(2);
+//   let length = Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / 10);
+//   let x_shadow = Math.round(length * Math.cos(rad));
+//   let y_shadow = Math.round(length * Math.sin(rad));
+//   title.style.setProperty('--x-shadow', -x_shadow + 'px');
+//   title.style.setProperty('--y-shadow', -y_shadow + 'px');
+// };
+
 const PageNotFound = () => {
   return (
     <PageNotFd>
