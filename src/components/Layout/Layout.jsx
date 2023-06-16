b059977f51c25ea6ebc850be591bf8660b6e9931
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-06-16 16:31:17 +0300

    add container

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { Container } from './Layout.styled';
+import { CommonContainer } from './Layout.styled';
 import { Suspense } from 'react';
 import Header from '../Header/Header';
 // import { Loader } from '../Loader/Loader';
@@ -7,14 +7,14 @@ import Header from '../Header/Header';
 export const Layout = () => {
   return (
     <>
-      <Container>
+      <CommonContainer>
         <Header />
         <main>
           <Suspense fallback={<div>Loading....</div>}>
             <Outlet />
           </Suspense>
         </main>
-      </Container>
+      </CommonContainer>
     </>
   );
 };
