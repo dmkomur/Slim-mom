e308b72d78e442e96ace506f2b5a8a8650ae315c
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-06-16 13:06:27 +0300

    Added diggerent format img

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -9,6 +9,12 @@ import {
 } from './PageNotFound.styled';
 import { createRoot } from 'react-dom/client';
 import strawberry from 'img/Strawberry-desktop@2x-min.png';
+import strawberry_png_d from 'img/Strawberry-desktop@2x-min.png';
+import strawberry_webp_d from 'img/Strawberry-desktop@2x-min.webp';
+import strawberry_avif_d from 'img/Strawberry-desktop@2x-min.avif';
+import strawberry_png_t from 'img/main-strawberry-tablet@2x-min.png';
+import strawberry_webp_t from 'img/main-strawberry-tablet@2x-min.webp';
+import strawberry_avif_t from 'img/main-strawberry-tablet_2x-min.avif';
 
 const root = createRoot(document.getElementById('root'));
 function PageNotFound() {
@@ -18,12 +24,63 @@ function PageNotFound() {
         <li>4</li>
         <li>
           <StyledSpan>
-            {/* <picture>
+            <picture>
+              <source
+                type="image/avif"
+                srcset={`${strawberry_avif_d} 1x, ${strawberry_avif_d} 2x`}
+                media="(min-width: 1200px)"
+              />
+              <source
+                type="image/webp"
+                media="(min-width: 1200px)"
+                srcset={`${strawberry_webp_d} 1x, ${strawberry_webp_d} 2x`}
+              />
               <source
                 type="image/png"
-                srcset={(${strawberry})}
-               
                 media="(min-width: 1200px)"
+                srcset={`${strawberry_png_d} 1x, ${strawberry_png_d} 2x`}
+              />
+              <source
+                type="image/avif"
+                width="100"
+                height="100"
+                srcset={`${strawberry_avif_t} 1x, ${strawberry_avif_t} 2x`}
+                media="(min-width: 768px)"
+              />
+              <source
+                type="image/webp"
+                width="100"
+                height="100"
+                media="(min-width: 768px)"
+                srcset={`${strawberry_webp_t} 1x, ${strawberry_webp_t} 2x`}
+              />
+              <source
+                type="image/png"
+                width="100"
+                height="100"
+                media="(min-width: 768px)"
+                srcset={`${strawberry_png_t} 1x, ${strawberry_png_t} 2x`}
+              />
+              <source
+                type="image/avif"
+                width="75"
+                height="75"
+                srcset={`${strawberry_png_t} 1x, ${strawberry_png_t} 2x`}
+                media="(min-width: 320px)"
+              />
+              <source
+                type="image/webp"
+                width="75"
+                height="75"
+                media="(min-width: 320px)"
+                srcset={`${strawberry_png_t} 1x, ${strawberry_png_t} 2x`}
+              />
+              <source
+                type="image/png"
+                width="75"
+                height="75"
+                media="(min-width: 320px)"
+                srcset={`${strawberry_png_t} 1x, ${strawberry_png_t} 2x`}
               />
               <StyledImg
                 src={strawberry}
@@ -31,14 +88,14 @@ function PageNotFound() {
                 width="150"
                 height="150"
               ></StyledImg>
-            </picture> */}
+            </picture>
           </StyledSpan>
         </li>
         <li>4</li>
       </StyledHeader>
       <StyledLine>Page not found...</StyledLine>
-      <div class="container">
-        <p class="text">
+      <div className="container">
+        <p className="text">
           <StyledText>
             Sorry, the page you're looking for cannot be accessed
           </StyledText>
