7191a6fb9698b4712c332963ca7761f5d6007ff2
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-06-16 19:39:51 +0300

    add nav and userinfo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,43 @@
-function Navigation() {
-  return <div>Navigation</div>;
-}
+import { NavLink } from 'react-router-dom';
+import { getIsLoggedIn } from '../../redux/auth/auth-selectors';
+import UserInfo from '../UserInfo';
+import { useSelector } from 'react-redux';
+import { NavigationList, NavigationItem, NavItem, NavigationLogin, NavigationDiary, StyledUserInfo,NavigationListDiary } from './Navigation.styled';
 
+function Navigation() {
+  const isUserLogin = useSelector(getIsLoggedIn);
+
+  return (
+    <div>
+      <nav>
+        {!isUserLogin ? (
+          <NavigationLogin>
+          <NavigationList>
+            <NavigationItem>
+              <NavItem to="login">Login</NavItem>
+            </NavigationItem>
+            <NavigationItem>
+              <NavItem to="registration">Registration</NavItem>
+            </NavigationItem>
+          </NavigationList>
+          </NavigationLogin>
+        ) : (
+          <NavigationDiary>
+          <NavigationListDiary>
+            <NavigationItem>
+              <NavItem to="diary">Diary</NavItem>
+            </NavigationItem>
+            <NavigationItem>
+              <NavItem to="calculator">Calculator</NavItem>
+            </NavigationItem>
+          </NavigationListDiary>
+        {<StyledUserInfo><UserInfo /></StyledUserInfo>}
+        </NavigationDiary>
+        )}
+      </nav>
+    </div>
+  );
+}
 export default Navigation;
+
+
