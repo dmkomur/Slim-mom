becdd142f070dc9e9497d1eca518fba9da28592d
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-06-18 16:07:16 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Suspense, useEffect, lazy } from 'react';
 import { getUser, refreshUser } from 'redux/auth/auth-operations';
 import { Route, Routes } from 'react-router-dom';
 import { PublicRoute } from './PublicRoute';
 import { PrivateRoute } from './PrivateRoute';
 import { Layout } from './Layout/Layout';
 import 'react-toastify/dist/ReactToastify.css';
-import { getIsRefreshing } from 'redux/auth/auth-selectors';
+// import { getIsRefreshing } from 'redux/auth/auth-selectors';
 import { ThemeSwitching } from './styles/ThemeSwitching';
 import { useAuth } from 'hooks';
 import { GlobalStylesPrivate } from './styles/GlobalStylePrivate.styled';
 import { GlobalStylePublic } from './GlobalStylePublic/GlobalStylePublic.styled';
 import Loader from './Loader/Loader';
-import Login from 'pages/Login';
+// import Login from 'pages/Login';
 import Register from 'pages/Register';
 import Home from 'pages/Home';
 import Calculator from 'pages/Calculator';
 import Diary from 'pages/Diary';
 import PageNotFound from './PageNotFound/PageNotFound';
+
+const Login = lazy(() => import('pages/Login'));
 
 export const App = () => {
   const { isLoggedIn } = useAuth();
@@ -28,55 +30,52 @@ export const App = () => {
       .then(() => dispatch(getUser()));
   }, [dispatch]);
 
-  const isRefreshing = useSelector(getIsRefreshing);
+  // const isRefreshing = useSelector(getIsRefreshing);
 
-  return isRefreshing ? (
-    <Loader />
-  ) : (
+  return (
     <>
       <ThemeSwitching>
         {isLoggedIn ? <GlobalStylesPrivate /> : <GlobalStylePublic />}
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route
-              path="/registration"
-              element={
-                <PublicRoute restricted>
-                  <Register />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <PublicRoute restricted>
-                  {/* <Suspense fallback={<div>Loading...</div>}> */}
-                  <Login />
-                  {/* </Suspense> */}
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/calculator"
-              element={
-                <PrivateRoute>
-                  <Calculator />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/diary"
-              element={
-                <PrivateRoute>
-                  <Diary />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-        {/* </Suspense> */}
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route
+                path="/registration"
+                element={
+                  <PublicRoute restricted>
+                    <Register />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute restricted>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/calculator"
+                element={
+                  <PrivateRoute>
+                    <Calculator />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/diary"
+                element={
+                  <PrivateRoute>
+                    <Diary />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </ThemeSwitching>
     </>
   );
